e989d22c1927cd4dabbaacf283970f4922a290d2
phuongvyyy11 <117069678+phuongvyyy11@users.noreply.github.com>
2024-05-26 23:03:10 +0700

    Project2.sql

--- Project2.sql
+++ Project2.sql
@@ -115,6 +115,93 @@ b.product_category as product_categories,
 sum(a.sale_price) as revenue
 from bigquery-public-data.thelook_ecommerce.order_items as a
 join bigquery-public-data.thelook_ecommerce.inventory_items as b on a.product_id = b.product_id
-where a.status='Complete' and a.delivered_at between '2022-01-15 00:00:00' and '2022-04-16 00:00:00'
-group by format_date('%Y-%m-%d',a.delivered_at), b.product_category
-order by dates
+
+
+--III
+--dataset
+create or replace view vw_ecommerce_analyst as
+(
+with cte as 
+(
+select
+format_date('%Y-%m', a.created_at) as month,
+format_date('%Y', a.created_at) as year,
+b.category as product_category,
+sum(c.sale_price) as TPV,
+sum(c.order_id) as TPO,
+sum(b.cost) as total_cost,
+from bigquery-public-data.thelook_ecommerce.orders as a
+join bigquery-public-data.thelook_ecommerce.products as b on a.order_id = b.id
+join bigquery-public-data.thelook_ecommerce.order_items as c on b.id = c.order_id
+group by month, year, product_category
+)
+select month, year, product_category, TPV, TPO,
+round(100.00*(TPV-(lag(TPV) over (partition by product_category order by year, month)))/(lag(TPV) over (partition by product_category order by year, month)),2) || '%' as revenue_growth,
+round(100.00*(TPO-(lag(TPO) over (partition by product_category order by year, month)))/(lag(TPO) over (partition by product_category order by year, month)),2) || '%' as order_growth,
+total_cost,
+TPV-total_cost as total_profit,
+(TPV-total_cost)/total_cost as profit_to_cost_ratio
+from cte
+order by month, year, product_category
+)
+
+--cohort index
+With cte1 as (
+select 
+user_id, 
+sale_price as amount,
+min(created_at) over (partition by user_id) as first_purchase_date,
+created_at
+from bigquery-public-data.thelook_ecommerce.order_items
+),
+cte2 as (select 
+user_id, amount,
+format_date('%Y-%m', first_purchase_date) as cohort_month,
+(extract(year from created_at)-extract(year from first_purchase_date))*12 + (extract(month from created_at)-extract(month from first_purchase_date))+1 as index
+from cte1),
+cohort_data as (
+select 
+count(distinct user_id) as user_count,
+sum(amount) as revenue,
+cohort_month,
+index
+from cte2
+group by cohort_month, index
+order by index),
+
+--customer cohort
+user_cohort as (
+select 
+cohort_month,
+sum(case when index = 1 then user_count else 0 end) as m1,
+sum(case when index = 2 then user_count else 0 end) as m2,
+sum(case when index = 3 then user_count else 0 end) as m3,
+sum(case when index = 4 then user_count else 0 end) as m4
+from cohort_data
+group by cohort_month
+order by cohort_month
+),
+  
+--retention cohort
+retention_cohort as
+(select 
+cohort_month,
+(m1/m1)*100.00 || '%' as m1,
+round((m2/m1)*100.00,2) || '%' as m2,
+round((m3/m1)*100.00,2) || '%' as m3,
+round((m4/m1)*100.00,2) || '%' as m4
+from user_cohort
+)
+  
+--churn rate
+select 
+cohort_month,
+(100 - round((m1/m1)*100.00,2)) || '%' as m1,
+(100 - round((m2/m1)*100.00,2)) || '%' as m2,
+(100 - round((m3/m1)*100.00,2)) || '%' as m3,
+(100 - round((m4/m1)*100.00,2)) || '%' as m4
+from user_cohort
+
+
+
+
